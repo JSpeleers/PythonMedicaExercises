5f21b839dda265ce0bbd5aa1c67bcf0982dadf56
Jasper Speleers <jasper_speleers@msn.com>
2021-10-21 17:30:05 +0200

    Added Salary exercise

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# PythonMedicaExercises
+# Python Medica Exercises
+
+## Info
+
+Using Python 3.9.5 while shadowing the `input()` function to mimic Dodona.
+The contents of the `solution()` or `solution_X()` functions is the code which needs to be pasted into Dodona.
+The only noticable difference is that an `IndexError: list index out of range` in this code will mean an `EOFError: EOF when reading a line` in Dodona.
+
+
+## Solutions
+
+### Reeks 2
+
+- [Salary](./reeks2/salary.py)
---
+++ reeks2/salary.py
@@ -0,0 +1,37 @@
+input_line_number = 0
+lines = []
+
+
+def input():
+    global input_line_number
+    input_line_number += 1
+    return lines[input_line_number - 1]
+
+
+def solution():
+    # Solution starts here
+    total = 0
+    index = 0
+    first_random_number = 0
+
+    while True:
+        salary = input()
+        if index == 0:
+            first_random_number = int(salary)  # First number is the random number
+            total = first_random_number
+        elif salary == 'stop':
+            break  # Stop looping when reaching final line in input
+        else:
+            total += int(salary)
+            print('worker #{} whispers €{}'.format(index, total))
+        index += 1
+
+    print('average salary: €{:.2f}'.format((total - first_random_number) / (index - 1)))
+    # Solution ends here
+
+
+if __name__ == '__main__':
+    with open('./inputs/input_salary.txt') as file:
+        lines = file.readlines()
+        lines = [line.rstrip() for line in lines]
+    solution()
